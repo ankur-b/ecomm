c67d61f5b181c8e5c81750dabc36203df0fce749
ankur-b <ankurbarve29@gmail.com>
2020-10-29 20:01:03 +0530

    implement signin and signup

--- src/firebase/firebaseutils.js
+++ src/firebase/firebaseutils.js
@@ -12,7 +12,24 @@ const firebaseConfig = {
     appId: "1:254376700126:web:24c185f75c8a9c7cb73a4f",
     measurementId: "G-XB365QQVTY"
   };
+  export const createUserProfileDocument = async (userAuth,additionalData) =>{
+    if (!userAuth) return;
 
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapshot = await userRef.get()
+    if(!snapshot.exists){
+      const {displayName, email} = userAuth
+      const createdAt = new Date()
+      try{
+        await userRef.set({
+          displayName,email,createdAt,...additionalData
+        })
+      }catch(error){
+        console.log('error creating user',error.message)
+      }
+    }
+    return userRef
+  }
   firebase.initializeApp(firebaseConfig)
 
   export const auth = firebase.auth()
